21a9e9815a6cc6000f311d178fe40ce7bbcfaa5e
nijamkhansherani <nijamkhansherani@gmail.com>
2023-03-25 10:58:24 +0530

    first commit

--- Routers/allcategories.js
+++ Routers/allcategories.js
@@ -10,7 +10,7 @@ router.get('/',(req,res)=>{
     res.json({status:true,Categories})
 })    
 router.get('/WDC',(req,res)=>{
-    res.json({status:true,data:Westerndresscollections})
+    res.json({status:true,Westerndresscollections})
 })  
 
 
