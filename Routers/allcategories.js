ba983c56d46df00a57b1b34ee3f236ea8316ea7e
nijamkhansherani <nijamkhansherani@gmail.com>
2023-03-27 23:21:44 +0530

    first commit

--- Routers/allcategories.js
+++ Routers/allcategories.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const {Categories,Westerndresscollections,Specialsareecollections, StylishKurtiCollections,TrendingKurtiCollections,FashionSareeCollections,TopSellingKurti,TopSareeCollectins,BridalWeddingCollection,Dressesforyou,
-    WesternDressesForWomen,StellarStylesForHim,WomenSareecollections,LongKurtiCollections,NewArrivalsTrousers  } = require('../data')
+    WesternDressesForWomen,StellarStylesForHim,WomenSareecollections,LongKurtiCollections,NewArrivalsTrousers ,
+    Swaiper1,Swaiper2,Swaiper3,Swaiper4,Swaiper5,Swaiper6 } = require('../data')
 
 const router = express.Router();
 
@@ -51,6 +52,28 @@ router.get('/LKC',(req,res)=>{
 })   
 router.get('/NAT',(req,res)=>{
     res.json({status:true,NewArrivalsTrousers})
+})
+
+
+router.get('/Swaiper1',(req,res)=>{
+    res.json({status:true,Swaiper1})
+})   
+router.get('/Swaiper2',(req,res)=>{
+    res.json({status:true,Swaiper2})
+})   
+router.get('/Swaiper3',(req,res)=>{
+    res.json({status:true,Swaiper3})
+})   
+router.get('/Swaiper4',(req,res)=>{
+    res.json({status:true,Swaiper4})
+})   
+router.get('/Swaiper5',(req,res)=>{
+    res.json({status:true,Swaiper5})
+})   
+router.get('/Swaiper6',(req,res)=>{
+    res.json({status:true,Swaiper6})
 })   
 
+
 module.exports = router;
+
