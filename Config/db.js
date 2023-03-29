4706ac0786d2d5d3f6c9b1538de91281ddac450d
nijamkhansherani <nijamkhansherani@gmail.com>
2023-03-29 10:09:44 +0530

    first commit3

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ConnectDB= async()=>{
     try{
         const connect = await mongoose.connect('mongodb+srv://nijamakhan373:zJ1xO3xWnwEDene3@nijam.zrcpiun.mongodb.net/test')
-       // console.log(connect);
+        console.log(connect);
 
     }
     catch(error){
